9c9df6a8a0ed47fcb2d861008065e27d83eddca6
trash-king <gapepin23@gmail.com>
2024-10-24 12:59:47 -0700

    Initial Commit
    Initial Commit for PyCalculator. Includes all files, main.py, and basic calculator functionality.

---
+++ standard_function.py
@@ -0,0 +1,19 @@
+#---------- Functions ----------#
+#TODO Add Scientific andf Graphing functions
+#TODO organize into multiple .py files, one for Standard, one for Graphing, and one for Scientific
+def addTwoIntegers():
+    print("add two")
+def subtractTwoIntegers():
+    print("subtract two")
+def divideTwoIntegers():
+    print("divide two")
+def multiplyTwoIntegers():
+    print("multiply two")
+def setNegative():
+    print("negative")
+def setExponent():
+    print("exponent")
+def findPercentage():
+    print("percent")
+def findModulus():
+    print("modulus")
---
+++ target.py
@@ -0,0 +1,5 @@
+from graphing_functions import *
+from standard_function import *
+from scientific_functions import *
+
+version = "1.0.0"
